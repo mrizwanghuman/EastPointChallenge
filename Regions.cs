585b1f32b927b0f6b0d651e8873371719913eba8
Muhammad Rizwan <mrizwanghuman@yahoo.com>
2016-10-13 10:22:05 -0400

    Uploaded file again

---
+++ Regions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Regions
+    {
+        public string[] RegionName { get; set; }
+
+        public Regions() {
+                               
+            ZipGroups zipgroups = new ZipGroups();
+            RegionName = new[] { zipgroups.zipCodesGroup1, zipgroups.zipCodesGroup2 };
+           
+
+        }
+        T[] InitializeArray<T>(int length) where T : new()
+        {
+            T[] array = new T[length];
+            for (int i = 0; i < length; ++i)
+            {
+                array[i] = new T();
+            }
+
+            return array;
+        }
+    }
+}
---
+++ ZipGroups.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class ZipGroups
+    {
+        public int[] zipCodesGroup1 { get; set; }
+        public int zipCodesGroup2 { get; set; }
+      
+      public ZipGroups()
+      {
+          int[] zipCodesGroup1 = new int[3] { 06108, 06109, 06110 };
+
+          int zipCodesGroup2 = new int { zipCodesGroup2 };
+          
+      }
+    
+
+      
+    }
+}
